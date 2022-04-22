bb15df53698f2a1176ae95a392fe7a0802b07ac9
PSAI222 <103354654+PSAI222@users.noreply.github.com>
2022-04-22 12:32:30 +0530

    Add files via upload

---
+++ js/music-list.js
@@ -0,0 +1,40 @@
+let allMusic = [
+
+    {
+        name: "naa kosam - bangarraju song",
+        artist: "Anup Rubens",
+        img: "music-1",
+        src: "music-1"
+    },
+    {
+        name: "Luddunda - bangarraju song",
+        artist: "Anup Rubens",
+        img: "music-2",
+        src: "music-2"
+    },
+    {
+        name: "vaasivaadi-tassadiyya - bangarraju song",
+        artist: "Anup Rubens",
+        img: "music-3",
+        src: "music-3"
+    },
+    {
+        name: "Bangaara - bangarraju song",
+        artist: "Anup Rubens",
+        img: "music-4",
+        src: "music-4"
+    },
+    {
+        name: "entha sakkagunnavro - bangarraju song",
+        artist: "Anup Rubens",
+        img: "music-5",
+        src: "music-5"
+    },
+    {
+        name: "nuvvu siggupadithe - bangarraju song",
+        artist: "Anup Rubens",
+        img: "music-6",
+        src: "music-6"
+    },
+    
+    ];
